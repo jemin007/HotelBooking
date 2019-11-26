b228c8a2da645b4f31e3c13701a9229f0b8cb58b
jemin007 <jeminshrestha9@gmail.com>
2019-11-26 17:28:36 +0545

    Initial commit

---
+++ app/src/main/java/com/kathmandu/datecheckin/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kathmandu.datecheckin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
